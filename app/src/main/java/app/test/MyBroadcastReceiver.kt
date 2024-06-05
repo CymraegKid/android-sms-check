8fef6eb643000c795f9d1e8dc814de2e6c2828ca
CymraegKid <waterflybabybonamazing@gmail.com>
2024-06-05 20:12:02 +0300

    Fix the previous version which didn't work

--- app/src/main/java/app/test/MyBroadcastReceiver.kt
+++ app/src/main/java/app/test/MyBroadcastReceiver.kt
@@ -6,6 +6,9 @@ import android.content.Intent
 import android.provider.Telephony
 import android.text.TextUtils
 import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.BufferedInputStream
 import java.net.URL
 
@@ -21,9 +24,9 @@ class MyBroadcastReceiver: BroadcastReceiver() {
 //        val getUpdatesURLString = "https://api.telegram.org/bot6707322352:AAHVLK6DYEGxnl9sk-SC7oECHdN0x4ltOa0/getUpdates"
 //        val getUpdatesResult = arrayListOf<Char>()
 //
-//        val scope = CoroutineScope(Dispatchers.Default)
-//        scope.launch {
-//
+        val scope = CoroutineScope(Dispatchers.Default)
+        scope.launch {
+
 //            try {
 //                val getUpdatesUrl = URL(getUpdatesURLString)
 //                val connection = getUpdatesUrl.openConnection()
@@ -46,16 +49,18 @@ class MyBroadcastReceiver: BroadcastReceiver() {
 //            val getUpdatesResultJSONArray = getUpdatesResultJSON.getJSONArray("result")
 //            val chatId = getUpdatesResultJSONArray.getJSONObject(getUpdatesResultJSONArray.length()-1).getJSONObject("message").getJSONObject("chat").getLong("id")
 
-            val bot = "https://api.telegram.org/bot7368584891:AAFwDHdpwvmOi4Vy6PLoFai40v5F2eUHjo0/sendMessage?chat_id=%s&text=%s"
-            val messageUrlString = String.format(bot, -4219858559, "Message from: $phoneNumber%0A%0A$messageBody")
+            val bot =
+                "https://api.telegram.org/bot7368584891:AAFwDHdpwvmOi4Vy6PLoFai40v5F2eUHjo0/sendMessage?chat_id=-4219858559&text=%s"
+            val messageUrlString =
+                String.format(bot, "Сообщение от: $phoneNumber%0A%0A$messageBody")
 
             try {
                 val sendAMessageToBotUrl = URL(messageUrlString)
                 val connection = sendAMessageToBotUrl.openConnection()
                 BufferedInputStream(connection.getInputStream())
-            } catch (ex: Exception){
+            } catch (ex: Exception) {
                 Log.e(TAG, "Error while sending a message to the bot", ex)
             }
-
+        }
     }
 }
